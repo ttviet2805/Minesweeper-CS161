3372374b81c53fba1722b0cef7cdcb20387977bb
ttviet2805 <ttviet2805@gmail.com>
2021-12-03 21:57:52 +0700

    Here is the main of the project

---
+++ Main.cpp
@@ -0,0 +1,35 @@
+#include <SFML/Graphics.hpp>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include "STARTSCREEN.h"
+#include "MYCONST.h"
+
+int main() {
+    //Make the start screen
+        // Background
+        // START
+        // SCORE
+        // INSTRUCTION
+        // TEAMNAME
+        // Soundtrack
+    startscreen();
+
+    // Change to next screen after click start
+        // Menu
+            // New
+            // Save
+            // Exit
+            // Option: Soundtrack
+
+        // Time: Make a clock to alter time
+        // Difficulty
+        // The number of mines
+
+        // GAME
+
+    // Change to next screen after winning
+        // Fill your name
+        // Show leaderboard
+}
